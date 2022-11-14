319d0806ff875f5fb7f6e98fc128ad82f0c26dd4
kruffka <kruffko@gmail.com>
2022-11-14 04:45:14 +0300

    func8

--- 8_functions/function.c
+++ 8_functions/function.c
@@ -3,11 +3,15 @@
 int sum(int a, int b) {
 
     return a + b;
+
+    // тут
 }
 
 void test1(int a, int *p) {
+
     a = 5;
     *p = 5;
+
 }
 
 void test2(int N, int *arr) { // *arr or arr[]
@@ -18,28 +22,29 @@ void test3(int N, int arr[][3]) { // **arr - dynamic; arr[][size] - static
     arr[0][0] = 123;
 }
 
-void test4();
+void test4(int a, int b);
 
 void main() {
     
     int a = 1;
     int b = 1;
 
-    printf("my sum is %d\n", sum(a, b));
+    int sum_var = sum(a, b);
+    printf("my sum is %d %d\n", sum(a, b), sum_var);
 
 
-    // int *p = &b;
+    int *p = &b;
     test1(a, &b);
     printf("a = %d b = %d\n", a, b);
 
     const int N = 3;
     int arr_1d[N];
-    test2(N, arr_1d);
+    test2(N, arr_1d); // arr_1d == &arr_1d[0]
     printf("test2 = %d\n", arr_1d[0]);
 
 
     int arr_2d[N][N];
-    test3(N, arr_2d);
+    test3(N, arr_2d); // arr_2d == &arr_2d[0][0] == arr_2d[0]
     printf("test3 = %d\n", arr_2d[0][0]);
 
 
@@ -49,9 +54,12 @@ void main() {
 
     printf("fun_ptr res = %d\n", fun_ptr(a, b));
 
+
+    test4(0, 0);
+
 }
 
-void test4() {
+void test4(int a, int b) {
 
-    printf("This is %s\n", __FUNCTION__);
+    printf("This is %s %d %s\n", __FUNCTION__, __LINE__, __FILE__);
 }
